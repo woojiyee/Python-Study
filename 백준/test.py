aba9ee489d1fb890498e303800c2e441a7337257
woojiyee <77909833+woojiyee@users.noreply.github.com>
2022-12-01 00:44:15 +0900

    11/30(day 2)
    2nd commit

--- 백준/test.py
+++ 백준/test.py
@@ -9,14 +9,28 @@
 여러 줄 주석
 """
 
-# 줄 바꿈(개행) : \n
-#a = input("한자리 수 입력")
-#b = int(input("한자리 수 입력"))
+# print()
+# - 자동 개행 (기본값으로 end = "\n")
+# - 마즈막 출력 문자 바꾸고 싶으면 end 매개변수 이용
+#   ex) print 한 번하고 띄워쓰기만 하고 또 print를 원하면 print("출려할 내용", end = ' ')
 
-#print("a 자료형 :",type(a))
+# input
+
+""" for문 안에 input()을 쓸 때 for문 반복 횟수가 많으면 시간초과가 날 수 있음
+ -> 그런 경우 sys.stdin.readline()를 쓰면 시간초과가 나지 않음
+
+sys.stdin.readline()를 쓰려면 import sys를 해야함
+input("prompt message") : input()은 매개변수에 입력 시 띄울 안내문인 prompt message인 문자열 형식을 받음
+sys.stdin.readline() 매개변수에는 한번에 읽을 수 있는 글자 수 크기로 정수 자료형을 받음"""
+
+# 줄 바꿈(개행) : \n
+
+#a = input("한자리 수 입력")
+#print("a 자료형 :",type(a))s
 # input으로 받은 데이터는 문자열 형식이다!
 
-#print("b 자료형 :",type(b))
+# input으로 받은 데이터 정수형으로 쓰고 싶으면 형변환해줘야 함
+#b = int(input("한자리 수 입력"))
 
 #print("split() 결과의 자료형 :",input("자연수 두개 입력").split())
 #split() 결과는 list 형식
