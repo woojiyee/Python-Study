4a6b026b186a49b7507060782de47a4e402d8c43
woojiyee <77909833+woojiyee@users.noreply.github.com>
2022-12-02 23:28:23 +0900

    12/2( day 4 )
    commit 1

---
+++ 백준/B2_25501_재귀의 귀재.py
@@ -0,0 +1,55 @@
+# 1st
+'''# 테스트 케이스 개수
+T = int(input())
+
+# 문자열
+S = ''
+
+# 반환값과 함수 호출 횟수 리스트
+answer = [[0,0]]
+
+# 테스트 케이스 수만큼 문자열 입력받아 리스트에 넣기
+for i in range(T):
+    S = input()
+    
+    def recursion(s,l,r):
+        # recursion 함수 호출될 때마다 호출 횟수 증가
+        answer[i,1]+=1
+        if l>=r: return answer[i,0] = 1
+        elif s[i] != s[r]: return answer[i,0] = 0
+        else: return recursion(s, l+1, r-1)
+ '''       
+    
+
+#2nd
+# 테스트 케이스 개수
+T = int(input("테스트 케이스 수 입력:"))
+
+# 문자열
+S = ''
+
+
+# 테스트 케이스 수만큼 문자열 입력받아 리스트에 넣기
+for i in range(T):
+    s = input(" 문자열 입력:")
+    
+    # recursion 함수 호출 횟수
+    n = 0
+
+    def recursion(s,l,r):
+        # recursion 함수 호출될 때마다 호출 횟수 증가
+        n += 1
+
+        if l>=r: return 1 
+        elif s[i] != s[r]: return 0 
+        else: return recursion(s, l+1, r-1)
+
+    def isPalindrome(s):
+        return recursion(s, 0, len(s)-1)
+
+    print(isPalindrome(s),' ',n)
+
+
+    
+    
+ 
