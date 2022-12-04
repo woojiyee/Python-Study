c2535aad87efdc818b8727e07a20ff9d58d5a659
woojiyee <77909833+woojiyee@users.noreply.github.com>
2022-12-04 23:39:25 +0900

    12/2( day 4)
    commit 2

--- 백준/B2_25501_재귀의 귀재.py
+++ 백준/B2_25501_재귀의 귀재.py
@@ -23,7 +23,7 @@ for i in range(T):
 
 #2nd
 # 테스트 케이스 개수
-T = int(input("테스트 케이스 수 입력:"))
+'''T = int(input("테스트 케이스 수 입력:"))
 
 # 문자열
 S = ''
@@ -47,7 +47,40 @@ for i in range(T):
     def isPalindrome(s):
         return recursion(s, 0, len(s)-1)
 
-    print(isPalindrome(s),' ',n)
+    print(isPalindrome(s),' ',n)'''
+
+#3rd
+# 테스트 케이스 개수
+T = int(input("테스트 케이스 수 입력:"))
+
+# 문자열 리스트
+S = []
+
+
+def recursion(s,l,r):
+    global n
+    n +=1
+    # recursion 함수 호출될 때마다 호출 횟수 증가
+    if l>=r: return 1
+    elif s[l] != s[r]: return 0 
+    else: return recursion(s, l+1, r-1)
+
+def isPalindrome(s):
+    return recursion(s, 0, len(s)-1)
+
+
+# 테스트 케이스 수만큼 문자열 입력받아 리스트에 넣기
+for i in range(T):
+    S.append(input(" 문자열 입력:"))
+    
+for str in S:
+    # recursion 함수 호출 횟수
+    n = 0
+    print(isPalindrome(str),n)
+    
+
+    
+
 
 
     
