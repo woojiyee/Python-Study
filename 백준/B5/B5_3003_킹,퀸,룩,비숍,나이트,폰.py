14258c5ff7d673db53724f7093c19a0f2cad078d
woojiyee <77909833+woojiyee@users.noreply.github.com>
2022-12-22 22:31:30 +0900

    복습
    풀었던 문제들 다시 풀어보기
     이전엔 변수 하나 하나로 받고 print 안에 변수 여러개를 써서 계산했지만
     이번엔 데이터들을 리스트로 받아서 for문 돌려서 리스트의 요소마다 계산 후 출력

--- 백준/B5/B5_3003_킹,퀸,룩,비숍,나이트,폰.py
+++ 백준/B5/B5_3003_킹,퀸,룩,비숍,나이트,폰.py
@@ -10,7 +10,19 @@ pawn = int(pawn)
 
 print(1-king," ",1-queen," ",2-rook," ",2-bishop," ",2-knight," ",8-pawn)'''
 
-
+#2nd
+'''
 king, queen, rook, bishop, knight, pawn = map(int,input("킹, 퀸, 룩, 비숍, 나이트, 폰 순서로 개수 입력").split())
 
 print(1-king,1-queen,2-rook,2-bishop,2-knight,8-pawn)
+'''
+
+# 12.22 다시 풀어봄
+#1st
+
+chess = list(map(int,input().split()))
+print(chess)
+original = [1,1,2,2,2,8]
+
+for i in range(6):
+    print(original[i]-chess[i],end = ' ')
