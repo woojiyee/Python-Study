33eae673b99949efd72912639f79a938fa15409d
woojiyee <77909833+woojiyee@users.noreply.github.com>
2023-01-07 01:47:30 +0900

    1일 1문(필수)
    1/6.금
    이전에 dfs로 푼 문제 bfs로 풀기
    commit 1

--- 백준/S4/S4_16173_점프왕 쩰리.py
+++ 백준/S4/S4_16173_점프왕 쩰리.py
@@ -1,3 +1,4 @@
+# DFS & BFS 둘 다 풀 수 있는 문제
 '''
 문제 규칙
 1. 정사각형 구역 내부에서만 이동 가능
@@ -7,7 +8,7 @@
 5. 한번에 이동할 수 있는 칸의 수는 현재 밟고 있는 칸에 쓰여 있는 수 만큼 ( 칸에 쓰여있는 수 초과 또는 미만으로 이동 불가 )
 쩰리가 게임 구역에서 끝 점( 맨 오른쪽, 맨 아래칸)까지 도달 가능 여부를 알아보자!
 '''
-
+# DFS
 # 1st
 '''
 # 구역 크기
@@ -104,8 +105,7 @@ dfs(0,0,gameMap)
 '''
 
 # 3rd
-
-    
+'''    
 # 구역 크기
 n = int(input())
 
@@ -144,6 +144,7 @@ def dfs(y,x):
             check += 1
             if check == 2:
                 print("Hing")
+                return
         # 이동된 위치가 승리 지점이라면
         if gameMap[y][x] == -1:
             print("HaruHaru")
@@ -155,3 +156,102 @@ def dfs(y,x):
         
 
 dfs(0,0)
+'''
+
+
+
+# BFS
+'''
+# 1st
+from collections import deque
+
+# 게임 구역의 크기
+n = int(input("게임 구역의 크기:"))
+graph = [list(map(int,input("게임판 한줄씩 입력:").split())) for _ in range(n)]
+visited = [[False] * n for _ in range(n)]
+print("graph:",graph)
+print("graph[0][0]:",graph[0][0])
+print("visited:",visited)
+
+# 젤리 이동 방향
+# 아래로만 이동 가능
+dy = [1,0]
+# 우로만 이동 가능
+dx = [0,1]
+
+def bfs(y,x):
+    queue = deque()
+    queue.append((y,x))
+    while queue:
+        y,x = queue.popleft()
+        print("visited true 대입 전:",visited)
+        visited[y][x] = True
+        print("visited true 대입 전:",visited)
+        for i in range(2):
+            # 현재 밟고 있는 칸에 쓰여있는 수만큼 아래로 가는 경우
+            print(y,dy[i],graph[y][x])
+            ny = y + dy[i] * graph[y][x]
+            # 현재 밟고 있는 칸에 쓰여있는 수만큼 오른쪽으로 가는 경우
+            nx = x + dx[i] * graph[y][x]
+            if ny < 0 or ny >= n or nx <0 or nx >= n:
+                continue
+            # 마즈막 지점인 경우
+            if graph[ny][nx] == -1:
+                visited[ny][nx] = True
+                break
+            queue.append((ny,nx))
+
+bfs(0,0)
+print("bfs 후 visited:",visited)
+if visited[n-1][n-1] == True:
+    print("HaruHaru")
+else:
+    print("Hing")
+'''
+# 위 코드로 하면 백준서 메모리 초과 떠서 수정하다 맞춘 코드
+# BFS
+
+# Nst
+from collections import deque
+
+# 게임 구역의 크기
+n = int(input())
+graph = [list(map(int,input().split())) for _ in range(n)]
+visited = [[False] * n for _ in range(n)]
+
+# 젤리 이동 방향
+# 아래로만 이동 가능
+dy = [1,0]
+# 우로만 이동 가능
+dx = [0,1]
+
+def bfs(y,x):
+    queue = deque()
+    queue.append((y,x))
+    while queue:
+        y,x = queue.popleft()
+        visited[y][x] = True
+        for i in range(2):
+            # 현재 밟고 있는 칸에 쓰여있는 수만큼 아래로 가는 경우
+            ny = y + dy[i] * graph[y][x]
+            # 현재 밟고 있는 칸에 쓰여있는 수만큼 오른쪽으로 가는 경우
+            nx = x + dx[i] * graph[y][x]
+            if ny < 0 or ny >= n or nx <0 or nx >= n:
+                continue
+            if visited[ny][nx] == True:
+                continue
+            # 마즈막 지점인 경우
+            if graph[ny][nx] == -1:
+                visited[ny][nx] = True
+                return
+            queue.append((ny,nx))
+
+bfs(0,0)
+if visited[n-1][n-1] == True:
+    print("HaruHaru")
+else:
+    print("Hing")
+
+# 재방문하면 안 된다는 조건이 없어서 재방문해도 상관없을 거 같아서 
+# 재방문한 경우 continue하는 코드 안 했더니 '메모리 초과' 뜨고
+# 재방문 체킹하는 코드 추가 했더니 '맞았습니다' 뜸 ^^
