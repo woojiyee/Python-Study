19f514a675ff42dc7afe72f4af7d42993d4e20ea
woojiyee <77909833+woojiyee@users.noreply.github.com>
2023-03-03 23:32:08 +0900

    1일 1문
    3.3.금
    commit 2
    ( 다른 방법으로 풀어봄 )

--- 백준/S3/S3_2606_바이러스.py
+++ 백준/S3/S3_2606_바이러스.py
@@ -1,5 +1,5 @@
-# 1st
-
+# 1st (bfs 버전)
+'''
 from collections import deque
 
 # 컴퓨터 수
@@ -33,6 +33,42 @@ def bfs(start):
             q.append(i)
 
 bfs(1)            
+answer = visited.count(1)
+
+# 시작하는 노드는 감염된 노드로 치지 않는데
+# 시작하는 노드도 bfs 안 while문에서 visited = 1로 처리되어 count됨 -> -1 해주기
+print(answer - 1)'''
+
+# 백준 맞았습니다.
+
+# 2nd (dfs 버전)
+
+# 컴퓨터 수
+n = int(input())
+
+# 컴퓨터 연결 수
+m = int(input())
+
+# 인덱스를 1부터 시작
+graph = [[] for i in range(n+1)]
+visited = [0 for i in range(n+1)]
+# visited = [[0] for i in range(n+1)] 
+# 이렇게 썼더니 visited [[0],[0],..]로 되서
+# bfs 안 if문에서 if visited[i] != 0에서 [0] 은 0이 아니기에 다 continue 처리 됨 
+
+for i in range(m):
+    a, b = map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+def dfs(node):
+    if visited[node] == 0:
+        visited[node] = 1
+        for i in graph[node]:
+            dfs(i)
+
+dfs(1)
+
 answer = visited.count(1)
 
 # 시작하는 노드는 감염된 노드로 치지 않는데
