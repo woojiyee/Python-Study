8a62f7e20de85eb6b970cab9ee26cb89c9515786
woojiyee <77909833+woojiyee@users.noreply.github.com>
2023-03-29 20:47:07 +0900

    1일 1문
    3.28.화
    commit 2

--- 백준/S1/S1_1105_카드 구매하기.py
+++ 백준/S1/S1_1105_카드 구매하기.py
@@ -29,16 +29,96 @@ P1 = 3, P2= 5, P3 = 15, P4 = 16
 # 아이디어
 '''
 각 팩당 카드 1개의 가격이 큰 것을 우선시 산다.
+Q. P1 = 1, P2= 9, P3 = 15 P4 = 4
+    P3 을 사고 p1 *1를 사면 4개를 16원에 사는거니까 1개가 4원
+    P2 *2 사면 4개를 18원에 사는거고 한개당 4.5원
+    p3이 개당 5원이라 개당 최고가 팩이지만 최종적으로 최대 구매액은 아님
 '''
 
 # 1st
-
+'''
 # 구매해야하는 카드 n = 팩의 종류 수
 n = int(input())
 
 p = [int(input()) for _ in range(n)]
 
-p[0:0] = [0]
+# p[0:0] = [0] # 인덱스 0에 0이 들어감
+p.insert(0,0)
+
 
 # 각 카드팩에서의 카드 1개의 가격
-one = [p[i]//i for i in range(n)]
+one = [[p[i]//i,i] for i in range(1,n)]
+
+one.insert(0,[0,0])
+
+# 카드 하나의 가격이 가장 비싼 카드팩 먼저 구매
+one.sort()
+
+maxPay = []
+
+for i in range(1,n+1):
+    for onePay,cnt in one[i]:
+        if n % cnt == 0:
+            maxPay.append(onePay*n)
+        else:
+            pack = n // cnt
+            spare = n % cnt
+            for j in range(i+1,n+1):
+                print("계속 빙빙 돔")
+            maxPay.append(onePay*pack*cnt + one[spare])
+'''
+# 코드도 마무리 못 지음
+
+# 2nd
+'''
+# 구매해야하는 카드 n = 팩의 종류 수
+n = int(input())
+
+p = [int(input()) for _ in range(n)]
+
+# p[0:0] = [0] # 인덱스 0에 0이 들어감
+p.insert(0,0)
+
+dp = [0] * (n+1)
+
+for i in range(1,n+1):
+    dp[i] = max(dp[i-1])
+'''
+# dp[i]에 i개의 최대값을 저장해야하는건 알겠는데 규칙을 모르겠음
+
+# 3rd ( 블로그 )
+# 아이디어
+'''
+d[1] = p[1]
+d[2] = d[1] + p[1] or d[0] + p[2]
+d[3] = d[2] + p[1] or d[1] + p[2] or d[0] + p[3]
+d[4] = d[3] + p[1] or d[2] + p[2] or d[1] + p[3] or d[0] + p[4]
+'''
+# 구매해야하는 카드 n = 팩의 종류 수
+n = int(input())
+
+p = list(map(int, input().split()))
+
+'''
+p = [map(int, input().split())] 
+print(p) # [<map object at 0x104b5e680>]
+print(type(p)) # <class 'list'>
+# [()] 이런거 마냥 리스트 안에 하나의 맵이 있는 형식으로 된 듯
+# 문제에서는 map형식을 리스트형식으로 형변환해야하는데 []로는 형변환 된게 아니라 리스트로 감싸지기만 한듯
+'''
+p.insert(0,0)
+# p[0:0] = [0] # 인덱스 0에 0이 들어감
+# 한 줄로도 가능 p = [0] + list(map(int, input().split()))
+
+dp = [0] * (n+1)
+
+dp[1] = p[1]
+
+for i in range(2, n + 1):
+    for j in range(1, i + 1):
+        if dp[i] < dp[i - j] + p[j]:
+            dp[i] = dp[i - j] + p[j]
+        # dp[i] = max(dp[i], dp[i-k] + p[k])
+        # if 문을 한줄로도 표현 가능
+
+print(dp[n])
